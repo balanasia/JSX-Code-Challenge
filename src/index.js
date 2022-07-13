7e9016aca3010d124d321e78e62fa0b2d99807f2
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-13 16:51:02 -0500

    Add a div with an h1 and ul with 3 li

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Shopping List</h1>
+    <ul>
+      <li>Kitten Food</li>
+      <li>Cucumbers</li>
+      <li>Tea</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
